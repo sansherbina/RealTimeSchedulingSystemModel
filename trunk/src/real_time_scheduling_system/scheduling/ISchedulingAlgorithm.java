711d7ea6dd4383b46666a1e7c4e2a3d23e5346d3
chess_master <chess_master@i.ua>
2013-02-13 20:41:28 +0200

    as

---
+++ trunk/src/real_time_scheduling_system/scheduling/ISchedulingAlgorithm.java
@@ -0,0 +1,7 @@
+package real_time_scheduling_system.scheduling;
+
+import java.util.HashMap;
+
+public interface ISchedulingAlgorithm {
+	public HashMap<Integer, Integer> scheduleTask(Task[] tasks, Machine[] machines);
+}
